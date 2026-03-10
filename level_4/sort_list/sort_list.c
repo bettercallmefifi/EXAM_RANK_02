3dbb57b36e3eb6db3a14839417e607a083fa743f
bettercallmefifi <ferdaouskai@gmail.com>
2026-03-10 21:06:09 +0000

    add sort_list

---
+++ level_4/sort_list/sort_list.c
@@ -0,0 +1,23 @@
+#include "list.h"
+
+t_list *sort_list(t_list* lst, int (*cmp)(int, int))
+{
+	t_list *start;
+	int swap;
+
+	start = lst;
+	while(lst->next)
+	{
+		if (((*cmp)(lst-> data, lst->next->data)) == 0)
+		{
+			swap = lst->data;
+			lst->data = lst->next->data;
+			lst->next->data = swap;
+			lst = start;
+		}
+		else
+			lst = lst->next;
+	}
+	lst = start;
+	return (lst);
+}
